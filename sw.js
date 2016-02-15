78e52a39e2881e78ddd2fab7e034e35e0e0e6d2a
inian <inian1234@gmail.com>
2016-02-15 20:54:34 +0800

    ad

--- sw.js
+++ sw.js
@@ -4,7 +4,8 @@ var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
   '/engineers.sg/presenters.html',
   '/engineers.sg/assets/application-fb4db91d78f751f730cb08787d44431b053e157a8cbf135ed081f1b9ee6fab28.js',
-  '/engineers.sg/assets/application-90e67de72dc561524e714564f13f2c07f140e89d55383822a61fe6b2d8e7de68.css'
+  '/engineers.sg/assets/application-90e67de72dc561524e714564f13f2c07f140e89d55383822a61fe6b2d8e7de68.css',
+  '/cat.png'
 ];
 
 self.addEventListener('install', function(event) {
